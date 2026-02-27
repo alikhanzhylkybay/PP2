802e7294359040b7cd52bba453e01017498837ad
alikhanzhylkybay <zhylkybai13@gmail.com>
2026-02-27 13:30:02 +0500

    another version of the code

--- lab4/date.py/4.py
+++ lab4/date.py/4.py
@@ -2,3 +2,8 @@ import datetime
 d1 = datetime.datetime.strptime(input(),"%Y-%m-%d %H:%M:%S")
 d2 = datetime.datetime.strptime(input(),"%Y-%m-%d %H:%M:%S")
 print(int(abs(d2-d1).total_seconds()))
+
+#from datetime import datetime
+#d1 = datetime.fromisoformat(input())  # без шаблона
+#d2 = datetime.fromisoformat(input())
+#print(int(abs((d2 - d1).total_seconds())))
